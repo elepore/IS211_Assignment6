2e0f081c76e723454e7770837774a7bb7e19ac72
elepore <52303156+elepore@users.noreply.github.com>
2023-10-09 23:58:09 -0400

    Add files via upload

--- conversions.py
+++ conversions.py
@@ -1,13 +1,23 @@
-
 def convertCelsiusToKelvin(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
-    
+    kelvins = celsius + 273.15
     return kelvins
 
-
 def convertCelsiusToFahrenheit(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def convertFahrenheitToCelsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def convertFahrenheitToKelvin(fahrenheit):
+    kelvins = (fahrenheit + 459.67) * 5/9
+    return kelvins
+
+def convertKelvinToCelsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+def convertKelvinToFahrenheit(kelvin):
+    fahrenheit = (kelvin * 9/5) - 459.67
     return fahrenheit
